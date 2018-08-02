d54845935fac3c51838b8a0c790c2bbefd52b0f7
Peter Ellis Jones <pete@peterellisjones.com>
2018-08-02 20:06:59 +0300

    move basic functionality test

--- src/lib.rs
+++ src/lib.rs
@@ -92,3 +92,18 @@ fn perft_bench_starting_position(b: &mut test::Bencher) {
 
     b.iter(|| -> usize { perft(&mut position, 2) });
 }
+
+#[test]
+fn basic_functionality() {
+    let mut counter = MoveCounter::new();
+    let position = Position::from_fen(STARTING_POSITION_FEN).unwrap();
+
+    let in_check = legal_moves(&position, &mut counter);
+    assert!(!in_check);
+
+    assert_eq!(counter.moves, 20);
+    assert_eq!(counter.captures, 0);
+    assert_eq!(counter.castles, 0);
+    assert_eq!(counter.ep_captures, 0);
+    assert_eq!(counter.promotions, 0);
+}
